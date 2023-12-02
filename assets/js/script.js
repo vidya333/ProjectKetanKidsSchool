2164d459bf83d2c8f228b73f24c6a0525b7d1fd8
Vidya <vidya.nk07@gmail.com>
2023-12-02 17:58:47 +0530

    3

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,33 +1,11 @@
-<script src="https://cdn.jsdelivr.net/npm/swiper@10/swiper-bundle.min.js"></script>
-
-<script>
-  var swiper = new Swiper(".mySwiper", {
-    slidesPerView: "3",
-    spaceBetween: 24,
-    autoplay:{
-      delay:2000,
-      disabledOnInteraction:false,
-      pauseOnMouseEnter:false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-     
-    },
-  });
-
-  var swiper = new Swiper(".mySwiper1", {
-    slidesPerView: "2",
-    spaceBetween: 24,
-    autoplay:{
-      delay:3000,
-      disabledOnInteraction:false,
-      pauseOnMouseEnter:false,
-    },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-     
-    },
-  });
-</script>
+var sliderMain = document.querySelector('.slider-main');
+let item = sliderMain.getElementsByClassName('item');
+function next(){
+    sliderMain.append(item[0]);
+}
+function prev(){
+    sliderMain.prepend(item[item.length-1]);
+}
+setInterval(
+    next,1500
+)
